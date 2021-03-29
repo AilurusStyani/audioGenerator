550e2145180ca9cf3b267e0ad9750cd62843fd7d
AilurusStyani <boyce_bao@163.com>
2021-03-29 14:07:44 +0800

    V1.0
    seems work

--- stepSourceGenerator.m
+++ stepSourceGenerator.m
@@ -1,43 +1,74 @@
-function stepSourceGenerator(audioFreqMin,audioFreqMax,duration,steps)
-if nargin<1 || isempty(audioFreqMin)
-    audioFreqMin = 1000; % Hz
-end
-if nargin<2 || isempty(audioFreqMax)
-    audioFreqMax = 3000;% Hz
-end
-if nargin<3 || isempty(duration)
+function stepSourceGenerator(duration,steps,audioFreqMax,audioFreqMin,initial,terminal,testEnable)
+if nargin<1 || isempty(duration)
     duration = 2; % second
 end
-if nargin<4 || isempty(steps)
+if nargin<2 || isempty(steps)
     steps = 10;
 end
+if nargin <3 || isempty(audioFreqMax)
+    audioFreqMax = 1000;
+end
+if nargin <4 || isempty(audioFreqMin)
+    audioFreqMin = 500; % will be disable in pure tone ( steps == 1)
+end
+if nargin<5 || isempty(initial)
+    initial = 0.05;% second
+end
+if nargin<6 || isempty(terminal)
+    terminal = 0.05; % second
+end
+if nargin<7 || isempty(testEnable)
+    testEnable = true;
+end
+if initial > terminal
+    iniLonger = true;
+else
+    iniLonger = false;
+end
 
-sampleRate = 96000; % how many sample per second, Hz
+sampleRate = 96000; % how many sample per second, 1000 to 384000 Hz, typically 8000, 11025, 22050, 44100, 48000, and 96000 Hz
 % audioFrequency=linspace(audioFreqMin,audioFreqMax,sampleRate*duration);
 frequencies =  linspace(audioFreqMin,audioFreqMax,steps);
+
+% /: initial        _: mainbody      \: terminal
+% ////____\\           ////____\\
+%               ////____\\
+% or
+% //____\\\\           //____\\\\
+%               //____\\\\
+if iniLonger
+    bodyDuration = (duration-terminal)/steps-initial;
+else
+    bodyDuration = (duration-initial)/steps-terminal;
+end
+if bodyDuration < 0
+    error('terminal/initial too long');
+end
+
+y = zeros(round(sampleRate*duration),1); % matlab have some problam for big number, so I have to fun round() on it.
 for i = 1:steps
+    t = (1:round((bodyDuration+initial+terminal)*sampleRate))/sampleRate;
+    tempY = sin(frequencies(i)*2*pi.*t)';
+    initialAmp = linspace(0,1,sampleRate*initial);
+    terminalAmp = linspace(1,0,sampleRate*terminal);
+    tempY(1:length(initialAmp)) = tempY(1:length(initialAmp)).*initialAmp';
+    tempY(end-length(terminalAmp)+1:end) = tempY(end-length(terminalAmp)+1:end).*terminalAmp';
     if i == 1
-        t = (0:(duration/steps*sampleRate))/(duration/steps*sampleRate);
-        tempY = sin(frequencies(i)*2*pi.*t);
-        pointN = sampleRate/frequencies(i);
-        [~,index0] = max(tempY(end-pointN:end));
-        y = tempY(1:(end-pointN+index0));
+        y(1:round((bodyDuration+initial+terminal)*sampleRate)) = tempY;
     else
-        t = (1:(duration/steps*sampleRate))/(duration/steps*sampleRate);
-        tempY = sin(frequencies(i)*2*pi.*t+pi/2);
-        pointN = sampleRate/frequencies(i);
-        [~,index0] = max(tempY(end-pointN:end));
-        y=cat(2,y,tempY(1:(end-pointN+index0)));
+        if iniLonger
+            y((i-1)*(bodyDuration+initial)*sampleRate+1 : (i*(bodyDuration+initial)+terminal)*sampleRate) = y((i-1)*(bodyDuration+initial)*sampleRate+1 : (i*(bodyDuration+initial)+terminal)*sampleRate) +tempY;
+        else
+            y((i-1)*(bodyDuration+terminal)*sampleRate+1 : (i*(bodyDuration+terminal)+initial)*sampleRate) = y((i-1)*(bodyDuration+terminal)*sampleRate+1 : (i*(bodyDuration+terminal)+initial)*sampleRate)+tempY;
+        end
     end
 end
-% audioFrequency = sort(repmat(linspace(audioFreqMin,audioFreqMax,steps),1,round(duration*sampleRate/steps)));
-% temp = (1:sampleRate)'./sampleRate;
-% temp = repmat(temp,ceil(duration),1);
-% t = temp(1:length(audioFrequency));
-% y = sin(audioFrequency' * 2 * pi .* t);
 
-% plot(y)
-player = audioplayer([y, -y], sampleRate);
-player.play();
-% audiowrite('stepTone.wav', [y -y], sampleRate);
+player = audioplayer(y, sampleRate);
+if testEnable
+    plot(y)
+    player.play();
+    pause(duration);
+end
+audiowrite('stepTone.wav', y, sampleRate);
 end
